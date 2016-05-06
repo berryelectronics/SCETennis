bee24f9d13a06953bc51de06b42325b1aeaed46c
Matthias <freakywii@gmail.com>
2016-05-07 00:10:14 +0200

    Added slides

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
+        anchors: ['a-startseite', 'a-about-us', 'a-gallerie', 'a-mannschaft', 'a-terminkalender', 'a-trainingszeiten', 'a-mitglied-werden'],
         sectionsColor: ['#1bbc9b', '#4BBFC3'],
         loopBottom: true,
         afterRender: function () {
             setInterval(function () {
                 $.fn.fullpage.moveSlideRight();
-            }, 1000);
+            }, 5000);
         }
     });
 });
+
+/*
+$(window).on('scroll', function () {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = document.height();
+    
+    if (y_scroll_pos > scroll_pos_test) {
+
+    }
+});
+*/
