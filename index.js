a1d22cca9ec9bb9b71bfebcaf01608399d34b44c
DehV <david.brunschier@gmail.com>
2016-05-07 00:12:12 +0200

    Adjusted Color and timings

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3'],
+        sectionsColor: ['#1bbc9b', '#1E824C'],
         loopBottom: true,
         afterRender: function () {
             setInterval(function () {
                 $.fn.fullpage.moveSlideRight();
-            }, 1000);
+            }, 3500);
         }
     });
 });
