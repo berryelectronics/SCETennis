9769dc4c137faf928776b95ba9f8815afb2be206
Matthias <freakywii@gmail.com>
2016-05-06 22:43:28 +0200

    Auf Fullpage gewechselt

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-'use strict';
-
-$(function () {
-    $(".rslides").responsiveSlides();
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3'],
+        loopBottom: true,
+        afterRender: function () {
+            setInterval(function () {
+                $.fn.fullpage.moveSlideRight();
+            }, 1000);
+        }
+    });
 });
